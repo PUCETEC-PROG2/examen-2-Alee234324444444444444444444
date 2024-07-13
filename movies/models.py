a647afe994a235a3f0b3c361bd9375d4855772f9
Alee234324444444444444444444 <apbarrionuevo@puce.edu.ec>
2024-07-13 09:58:56 -0500

    La prueba esta compleatada

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    filmtitle = models.CharField(max_length=60, null=False)
+    filmgenre = models.CharField(max_length=30, null=False)
+    filmdirector = models.CharField(max_length=60, null=False)
+    filmpublicationyear = models.IntegerField(null=False)
+    synopsisofthefilm = models.TextField(null=False)
+    
+    def __str__(self) -> str:
+        return self.filmtitle
+    
+    
